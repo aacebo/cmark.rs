e56e097fb98acfdd5e8c9e34b0b0c3263dc5cf57
aacebo <aacebowork@gmail.com>
2025-07-07 12:25:48 -0400

    Update html_test.rs

--- htmlx_macros/tests/html_test.rs
+++ htmlx_macros/tests/html_test.rs
@@ -2,6 +2,13 @@ use htmlx::Element;
 
 #[test]
 pub fn should_render() {
-    let value = htmlx::rsx! { <div id={"myid"} /> };
-    assert_eq!(value.render(), r#"<div id="myid"/>"#);
+    let value = htmlx::html! { <div id={"myid"} /> };
+    assert_eq!(value, r#"<div id="myid"/>"#);
+}
+
+#[test]
+pub fn should_render_element() {
+    let id = "test";
+    let value = htmlx::rsx! { <div id={id} /> };
+    assert_eq!(value.render(), r#"<div id="test"/>"#);
 }
