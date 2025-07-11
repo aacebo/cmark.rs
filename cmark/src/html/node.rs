aa31fced9ac3b46e1f7c5cad4f8f9fef60c55417
aacebo <aacebowork@gmail.com>
2025-07-11 11:54:25 -0400

    add parse stream

--- cmark/src/html/node.rs
+++ cmark/src/html/node.rs
@@ -13,9 +13,19 @@ pub enum Node<'a> {
 impl<'a> Render for Node<'a> {
     fn render_into(&self, writer: &mut dyn std::fmt::Write) -> Result<(), std::fmt::Error> {
         return match self {
-            Node::Element(node) => node.render_into(writer),
-            Node::Raw(node) => node.render_into(writer),
-            Node::Other(node) => node.render_into(writer),
+            Self::Element(node) => node.render_into(writer),
+            Self::Raw(node) => node.render_into(writer),
+            Self::Other(node) => node.render_into(writer),
+        };
+    }
+}
+
+impl<'a> PartialEq for Node<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        return match self {
+            Self::Element(node) => node.render() == other.render(),
+            Self::Raw(node) => node.render() == other.render(),
+            Self::Other(node) => node.render() == other.render(),
         };
     }
 }
--- cmark/src/parsing/mod.rs
+++ cmark/src/parsing/mod.rs
@@ -1,2 +1,5 @@
 mod parser;
 pub use parser::*;
+
+mod stream;
+pub use stream::*;
